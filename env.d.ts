647142154c19697cc3dbec5aaa2fc82bb8e77882
AbsintheSung <h90243768@gmail>
2024-08-13 18:13:53 +0800

    fix:在env.d.ts添加配置

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+//配置 部屬前生成自動生成內容
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue'
+    const component: DefineComponent<{}, {}, any>
+    export default component
+  }
