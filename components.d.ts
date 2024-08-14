269dc213347679a93735b7fece6b7c7e503cf320
AbsintheSung <h90243768@gmail.com>
2024-08-15 00:00:50 +0800

    feat:配置container的左右padding

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ArouterLink: typeof import('./src/components/global/ArouterLink.vue')['default']
     AuthNavigation: typeof import('./src/components/global/AuthNavigation.vue')['default']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
