5eec81f67ce71ffaec89e84a70957941f7ea7048
misu13 <djmisu13@gmail.com>
2024-08-15 14:14:26 +0800

    feat:加入方案選擇區塊布局

--- src/components/plan-selection-page/PlanSelectionCard.vue
+++ src/components/plan-selection-page/PlanSelectionCard.vue
@@ -1,18 +1,33 @@
 <script setup>
-const plans = ['7餐', '14餐', '21餐']
 </script>
 <template>
     <div>我是header</div>
-    <!-- 套餐選擇區塊 -->
+    <!-- 方案天數選擇區塊 -->
     <div class="bg-el-primary flex justify-evenly text-4xl font-bold py-2">
         <button>7餐</button>
         <button>14餐</button>
         <button>21餐</button>
     </div>
-    <div class="container">
+    <!-- 方案選擇區塊 -->
+    <div class="container pt-20">
+        <div class="grid grid-cols-2 gap-6 text-center">
+            <div>
+                <img 
+                    src="https://s3-alpha-sig.figma.com/img/7f12/ea13/00756f144a0fb5daaf68dbfc01103a46?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JHhc4Q0WYH0I5cy4bml2i3ZUOxEhUp0XqvXKQerXRHWtsZTZZrizj63EZIeDpC8LKVhd8WyzmBDqMVjzPVtoBCnbJGAp9sasZQlfGnAtsuRLIHLc~heWKMBAtuTsfmYvX436gGnNNau2RXOQfKxh3qFXkAWoI-HLmi8ygOfYtx0Zmrl9pizTccAazXJt4GFBByKkQLgmSp2-zCAGG6PdvbOx8zh-OIBHbYUrDYMeWk-Zr1syEaCnEqSDPI1fdFZG59Zrp-srnhLuPGQfD~3Fi7yVR1r-5QB1alp4zPdVjn~Bfo5I5bk9VJQtOumEmMeP56lQHFuqvop41IDdAeNM4Q__"
+                    alt="營養師方案"
+                    >
+                <h3>營養師方案</h3>
+            </div>
+            <div> 
+                <img
+                    src="https://s3-alpha-sig.figma.com/img/7f12/ea13/00756f144a0fb5daaf68dbfc01103a46?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JHhc4Q0WYH0I5cy4bml2i3ZUOxEhUp0XqvXKQerXRHWtsZTZZrizj63EZIeDpC8LKVhd8WyzmBDqMVjzPVtoBCnbJGAp9sasZQlfGnAtsuRLIHLc~heWKMBAtuTsfmYvX436gGnNNau2RXOQfKxh3qFXkAWoI-HLmi8ygOfYtx0Zmrl9pizTccAazXJt4GFBByKkQLgmSp2-zCAGG6PdvbOx8zh-OIBHbYUrDYMeWk-Zr1syEaCnEqSDPI1fdFZG59Zrp-srnhLuPGQfD~3Fi7yVR1r-5QB1alp4zPdVjn~Bfo5I5bk9VJQtOumEmMeP56lQHFuqvop41IDdAeNM4Q__"
+                    alt="自定義方案"
+                    >
+                <h3>自定義方案</h3>
+            </div>
+        </div>
     </div>
     <div>我是footer</div>
 
 </template>
-<style scoped>
-</style>
+<style scoped></style>
