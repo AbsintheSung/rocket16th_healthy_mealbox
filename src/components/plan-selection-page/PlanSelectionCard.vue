c4ffd1fe14b2dab7a229c5fb6744988172b8be45
misu13 <djmisu13@gmail.com>
2024-08-15 12:18:18 +0800

    feat:加入產品選擇頁面布局

--- src/components/plan-selection-page/PlanSelectionCard.vue
+++ src/components/plan-selection-page/PlanSelectionCard.vue
@@ -1,7 +1,11 @@
 <script setup>
 </script>
 <template>
-    <div>我是選擇用的卡片:D</div>
+    <div>我是header</div>
+    <div class="bg-primary">71421</div>
+    <div class="container">
+    </div>
+    <div>我是footer</div>
+
 </template>
-<style scoped>
-</style>
+<style scoped></style>
