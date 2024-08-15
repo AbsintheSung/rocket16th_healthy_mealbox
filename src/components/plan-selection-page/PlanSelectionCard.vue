f3b652bd64bf4c11a5a3ddeb1d7f88d63a157aa5
misu13 <djmisu13@gmail.com>
2024-08-15 13:35:37 +0800

    feat:加入方案天數選擇區塊

--- src/components/plan-selection-page/PlanSelectionCard.vue
+++ src/components/plan-selection-page/PlanSelectionCard.vue
@@ -1,11 +1,18 @@
 <script setup>
+const plans = ['7餐', '14餐', '21餐']
 </script>
 <template>
     <div>我是header</div>
-    <div class="bg-primary">71421</div>
+    <!-- 套餐選擇區塊 -->
+    <div class="bg-el-primary flex justify-evenly text-4xl font-bold py-2">
+        <button>7餐</button>
+        <button>14餐</button>
+        <button>21餐</button>
+    </div>
     <div class="container">
     </div>
     <div>我是footer</div>
 
 </template>
-<style scoped></style>
+<style scoped>
+</style>
