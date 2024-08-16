f2ddb82494f23dae18020cf0e8761266a52fc4c7
AbsintheSung <h90243768@gmail>
2024-08-16 11:39:20 +0800

    refactor:將line&fb按鈕拆分成組件傳遞

--- src/components/register-page/RegisterForm.vue
+++ src/components/register-page/RegisterForm.vue
@@ -101,7 +101,7 @@ const handleRegister = async (formEl: FormInstance | undefined) => {
     <!-- <el-button type="primary" @click="handleRegister(ruleFormRef)">測試</el-button> -->
   </el-form>
   <el-divider><p style="color: #9cb0c9">OR</p></el-divider>
-  <div>
+  <!-- <div>
     <button
       class="relative my-5 flex w-full items-center justify-center rounded-md border border-[#358AF9] px-4 py-3 text-[#358AF9]"
     >
@@ -114,7 +114,7 @@ const handleRegister = async (formEl: FormInstance | undefined) => {
       <FontAwesomeIcon class="absolute left-4" :icon="['fab', 'line']" size="xl" />
       <p>使用Line註冊</p>
     </button>
-  </div>
+  </div> -->
 </template>
 <style scoped lang="scss">
 :deep(.el-form-item) {
--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,12 +1,38 @@
 <script lang="ts" setup>
 import RegisterForm from '@/components/register-page/RegisterForm.vue'
 import AuthNavigation from '@/components/global/AuthNavigation.vue'
+import ExternalAuthButton from '@/components/global/ExternalAuthButton.vue'
+const authButtonData = [
+  {
+    buttonClass: 'border border-[#358AF9] text-[#358AF9]',
+    buttonTitle: '使用Facebook註冊',
+    icon: ['fab', 'facebook'],
+    iconSize: 'xl',
+    handleButton: () => {
+      console.log('連結')
+    }
+  },
+  {
+    buttonClass: 'border border-[#58C067] text-[#58C067]',
+    buttonTitle: '使用Line註冊',
+    icon: ['fab', 'line'],
+    iconSize: 'xl',
+    handleButton: () => {
+      console.log('連結')
+    }
+  }
+]
 </script>
 <template>
   <main class="container">
     <div class="mx-auto mt-20 w-full md:w-1/2">
       <AuthNavigation class="mb-12" />
       <RegisterForm />
+      <ExternalAuthButton
+        v-for="buttonOption in authButtonData"
+        :key="buttonOption.buttonTitle"
+        :buttonOption="buttonOption"
+      />
     </div>
   </main>
 </template>
