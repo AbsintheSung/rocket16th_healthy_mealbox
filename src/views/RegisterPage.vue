1a58ea2b85a99cc29debbea44608dce6753970bf
AbsintheSung <h90243768@gmail>
2024-08-16 09:09:04 +0800

    update:調整註冊整體表單位置&間距

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -3,8 +3,8 @@ import RegisterForm from '@/components/register-page/RegisterForm.vue'
 import AuthNavigation from '@/components/global/AuthNavigation.vue'
 </script>
 <template>
-  <main class="container flex flex-col items-center justify-center">
-    <div class="w-full md:w-1/2">
+  <main class="container">
+    <div class="mx-auto mt-20 w-full md:w-1/2">
       <AuthNavigation class="mb-12" />
       <RegisterForm />
     </div>
