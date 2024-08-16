8dca4eda3dd5713bfdfe16f479536f438fd581fc
AbsintheSung <h90243768@gmail>
2024-08-16 09:01:25 +0800

    feat:更新整個表單的位置跟間距

--- src/views/SigninPage.vue
+++ src/views/SigninPage.vue
@@ -4,8 +4,8 @@ import AuthNavigation from '@/components/global/AuthNavigation.vue'
 </script>
 <template>
   <main class="container">
-    <div class="w-1/2">
-      <AuthNavigation />
+    <div class="mx-auto mt-20 w-full md:w-1/2">
+      <AuthNavigation class="mb-12" />
       <SigninForm />
     </div>
   </main>
