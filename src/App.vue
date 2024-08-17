7a7562a2ed2925a5ac970c8ea5d3ead59680bd8d
AbsintheSung <h90243768@gmail.com>
2024-08-17 13:48:52 +0800

    feat:配置footer畫面及樣式

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import HeaderLayout from '@/components/global/HeaderLayout.vue'
+import FooterLayout from '@/components/global/FooterLayout.vue'
 </script>
 
 <template>
   <HeaderLayout />
   <RouterView />
+  <FooterLayout />
 </template>
 
 <style scoped></style>
---
+++ src/components/global/FooterLayout.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+
+const mainNav = ref([
+  { id: 'accoumt', title: '我的帳戶', path: '/' },
+  { id: 'faq', title: '常見問題', path: '/' },
+  { id: 'privacy-policy', title: '隱私權保護政策', path: '/' }
+])
+const utilNav = ref([
+  { id: 'ig', iconStyle: ['fab', 'instagram'], path: '/' },
+  { id: 'fb', iconStyle: ['fab', 'facebook'], path: '/' },
+  { id: 'line', iconStyle: ['fab', 'line'], path: '/' }
+])
+</script>
+<template>
+  <footer class="bg-[#286D33]">
+    <div class="container py-6">
+      <div class="flex items-center justify-between text-[#FFFFFF]">
+        <ul class="flex">
+          <li v-for="mainNavItem in mainNav" :key="mainNavItem.id" class="py-1">
+            <RouterLink class="block px-2" :to="`${mainNavItem.path}`" :id="mainNavItem.id">
+              {{ mainNavItem.title }}
+            </RouterLink>
+          </li>
+        </ul>
+        <ul class="flex items-center justify-between">
+          <li v-for="utilNavItem in utilNav" :key="utilNavItem.id" class="py-1">
+            <RouterLink class="block px-2" :to="`${utilNavItem.path}`">
+              <FontAwesomeIcon :icon="utilNavItem.iconStyle" size="xl" />
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
+<style scoped lang="scss">
+#faq {
+  border: 1px solid #ffffff;
+  border-top: none;
+  border-bottom: none;
+}
+</style>
