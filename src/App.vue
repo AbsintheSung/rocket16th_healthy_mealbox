9df8595e48ad7c036e84daced970d87756f5555e
AbsintheSung <h90243768@gmail.com>
2024-08-17 11:22:45 +0800

    feat:建立header基礎布局

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import HeaderLayout from '@/components/global/HeaderLayout.vue'
 </script>
 
 <template>
+  <HeaderLayout />
   <RouterView />
 </template>
 
---
+++ src/components/global/HeaderLayout.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+<template>
+  <header>
+    <nav class="container">
+      <div class="flex items-center justify-between">
+        <ul class="flex">
+          <li></li>
+          <li>使用方式</li>
+          <li>關於我們</li>
+          <li>常見問題</li>
+        </ul>
+        <ul class="flex">
+          <li>搜尋icon</li>
+          <li>會員icon</li>
+          <li>購物車icon</li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+<style scoped></style>
