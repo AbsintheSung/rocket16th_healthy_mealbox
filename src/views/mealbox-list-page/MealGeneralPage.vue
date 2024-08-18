4d3f3b6ff18666c2190ea00ebbde27f065601845
AbsintheSung <h90243768@gmail.com>
2024-08-19 00:44:37 +0800

    fix:部屬失敗-修正檔名大小寫不一致

--- src/views/mealbox-list-page/MealGeneralPage.vue
+++ src/views/mealbox-list-page/MealGeneralPage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import MealCard from '@/components/global/mealCard.vue'
+import MealCard from '@/components/global/MealCard.vue'
 </script>
 <template>
   <ul class="grid grid-cols-2 gap-x-6 gap-y-12 md:grid-cols-3 lg:grid-cols-4">
