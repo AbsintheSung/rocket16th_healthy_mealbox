d0489dcb7d8b38fb48caafeef1148884fa860f1a
fire0607 <stu00634@gmail.com>
2024-08-18 22:54:35 +0800

    feat:加入卡片初步版型

--- src/components/nutritionist-plan-page/NutritionistPlanCard.vue
+++ src/components/nutritionist-plan-page/NutritionistPlanCard.vue
@@ -6,15 +6,16 @@ defineProps({
 })
 </script>
 <template>
-<div class="">
-  <h3>{{ title }}</h3>
-  <div>
-    <img :src="src" :alt="title" >
-    <div class="flex justify-end items-center">
-      <button>加入購物車</button>
-      <a href="#" class="flex items-center"><p>查看詳細</p><font-awesome-icon :icon="['fas', 'arrow-right']" /></a>
+<div class="flex flex-col">
+  <h3 class="text-2xl font-medium pb-6">{{ title }}</h3>
+  <div class="w-full">
+    <img :src="src" :alt="title" class="w-full h-[179px] object-cover">
+    <div class="flex justify-end items-center bg-primary-base py-4">
+      <div class="pr-4"><el-button>加入購物車</el-button></div>
+      <a href="#" class="flex items-center text-2xl text-white"><p class="pr-4">查看詳細</p><font-awesome-icon :icon="['fas', 'arrow-right']" class="pr-4"/></a>
     </div>
   </div>
 </div>
 </template>
-<style scoped></style>
+<style scoped>
+</style>
