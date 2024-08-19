5388c2ab8418184ed4691027f26632b4398ea6a3
fire0607 <stu00634@gmail.com>
2024-08-20 01:57:01 +0800

    update:更新營養師方案選擇頁路由

--- src/components/nutritionist-plan-page/NutritionistPlanCard.vue
+++ src/components/nutritionist-plan-page/NutritionistPlanCard.vue
@@ -6,16 +6,18 @@ defineProps({
 })
 </script>
 <template>
-<div class="flex flex-col">
-  <h3 class="text-2xl font-medium pb-6">{{ title }}</h3>
-  <div class="w-full">
-    <img :src="src" :alt="title" class="w-full h-[179px] object-cover">
-    <div class="flex justify-end items-center bg-primary-base py-4">
-      <div class="pr-4"><el-button>加入購物車</el-button></div>
-      <a href="#" class="flex items-center text-2xl text-white"><p class="pr-4">查看詳細</p><font-awesome-icon :icon="['fas', 'arrow-right']" class="pr-4"/></a>
+  <div class="flex flex-col">
+    <h3 class="text-2xl font-medium pb-6">{{ title }}</h3>
+    <div class="w-full">
+      <img :src="src" :alt="title" class="w-full h-[179px] object-cover">
+      <div class="flex justify-end items-center bg-primary-base py-4">
+        <div class="pr-4"><el-button>加入購物車</el-button></div>
+        <RouterLink class="flex items-center text-2xl text-white" to="/nutritionist-plan-detail">
+          <p class="pr-4">查看詳細</p><font-awesome-icon :icon="['fas', 'arrow-right']" class="pr-4" />
+        </RouterLink>
+
+      </div>
     </div>
   </div>
-</div>
 </template>
-<style scoped>
-</style>
+<style scoped></style>
