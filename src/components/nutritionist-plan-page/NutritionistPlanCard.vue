c0cbdcbfdf82abf60280dce2399f401be5f8128d
misu13 <djmisu13@gmail.com>
2024-08-20 14:26:28 +0800

    update:更新營養師方案加入購物車頁面按鈕

--- src/components/nutritionist-plan-page/NutritionistPlanCard.vue
+++ src/components/nutritionist-plan-page/NutritionistPlanCard.vue
@@ -11,7 +11,7 @@ defineProps({
     <div class="w-full">
       <img :src="src" :alt="title" class="w-full h-[179px] object-cover">
       <div class="flex justify-end items-center bg-primary-base py-4">
-        <div class="pr-4"><el-button>加入購物車</el-button></div>
+        <div class="pr-4"><button class="hover:shadow-base transition active:shadow-none">加入購物車</button></div>
         <RouterLink class="flex items-center text-2xl text-white" to="/nutritionist-plan-detail">
           <p class="pr-4">查看詳細</p><font-awesome-icon :icon="['fas', 'arrow-right']" class="pr-4" />
         </RouterLink>
