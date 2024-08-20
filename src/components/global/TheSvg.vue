13647e49345941cf10949192bf191454838cf626
AbsintheSung <h90243768@gmail.com>
2024-08-21 01:11:45 +0800

    feat:配置svg組件

---
+++ src/components/global/TheSvg.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import sprite from '@/assets/image/sprite.svg'
+const props = defineProps({
+  svgIcon: {
+    title: String,
+    default: '', //預設值
+    required: true //若為true，代表傳進來的 props 一定要有此資料
+  }
+})
+const svgStyle = props.svgIcon
+</script>
+<template>
+  <svg>
+    <use :href="`${sprite}#${svgStyle}`"></use>
+  </svg>
+</template>
+<style scoped></style>
