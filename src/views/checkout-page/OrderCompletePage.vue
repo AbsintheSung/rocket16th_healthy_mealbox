eb0d9342d9ad76c7c258146301c67cbf895807e6
fire0607 <stu00634@gmail.com>
2024-08-21 00:32:44 +0800

    feat:加入訂單確認頁面布局

--- src/views/checkout-page/OrderCompletePage.vue
+++ src/views/checkout-page/OrderCompletePage.vue
@@ -1,7 +1,14 @@
 <script setup>
 </script>
 <template>
-    <div></div>
+    <div class="grid grid-cols-12 gap-6">
+        <div class="col-span-8">
+            我是左邊
+        </div>
+        <div class="col-start-9 col-span-3">
+            我是右邊
+        </div>
+    </div>
 </template>
 <style scoped>
 </style>
