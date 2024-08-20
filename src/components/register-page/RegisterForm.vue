69f833ef3839f21fbaa1df1c2469c1bdfdeee8ed
AbsintheSung <h90243768@gmail.com>
2024-08-20 23:42:09 +0800

    feat:建立註冊邏輯&api管理(暫定)

--- src/components/register-page/RegisterForm.vue
+++ src/components/register-page/RegisterForm.vue
@@ -1,10 +1,22 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import type { FormInstance } from 'element-plus'
+import axios from '@/utils/apis/axiosInterceptors'
+import { useRouter, type Router } from 'vue-router'
+import { registerApi } from '@/utils/apis/apiurl'
 const ruleFormRef = ref<FormInstance>()
-const registerInput = ref({
-  userEmail: '',
-  userPassWord: '',
+const isLoading = ref<Boolean>(false)
+const router: Router = useRouter()
+type RegisterInputType = {
+  account: string
+  password: string
+  checkPassWord: string
+  privacy: string[]
+  newSletter: string[]
+}
+const registerInput = ref<RegisterInputType>({
+  account: '',
+  password: '',
   checkPassWord: '',
   privacy: [],
   newSletter: []
@@ -13,7 +25,7 @@ const validatePass = (rule: any, value: any, callback: any) => {
   if (value === '') {
     callback(new Error('請輸入密碼'))
   } else {
-    if (registerInput.value.userPassWord !== '') {
+    if (registerInput.value.password !== '') {
       if (!ruleFormRef.value) return
       ruleFormRef.value.validateField('checkPassWord')
     }
@@ -23,7 +35,7 @@ const validatePass = (rule: any, value: any, callback: any) => {
 const validatePass2 = (rule: any, value: any, callback: any) => {
   if (value === '') {
     callback(new Error('請輸入密碼'))
-  } else if (value !== registerInput.value.userPassWord) {
+  } else if (value !== registerInput.value.password) {
     callback(new Error('密碼與原先不符合'))
   } else {
     callback()
@@ -31,7 +43,7 @@ const validatePass2 = (rule: any, value: any, callback: any) => {
 }
 
 const registerRules = ref({
-  userEmail: [
+  account: [
     {
       type: 'email',
       required: true,
@@ -39,7 +51,7 @@ const registerRules = ref({
       trigger: ['blur', 'change']
     }
   ],
-  userPassWord: [
+  password: [
     { min: 6, max: 30, message: '長度介於6到30之間', trigger: 'blur' },
     { required: true, message: '必填', trigger: 'blur' },
     { validator: validatePass, trigger: 'blur' }
@@ -62,21 +74,54 @@ const handleRegister = async (formEl: FormInstance | undefined) => {
   if (!formEl) return
   await formEl.validate((valid, fields) => {
     if (valid) {
+      fetchRegister(registerInput.value)
       console.log('發送註冊API')
     } else {
       console.log('error submit!', fields)
     }
   })
-  console.log(registerInput.value)
+}
+const message = (mes: any, mesType: any): void => {
+  //@ts-ignore
+  ElMessage({
+    message: mes,
+    type: mesType,
+    duration: 1500
+  })
+}
+const fetchRegister = async (data: RegisterInputType) => {
+  const { account, password } = data
+  const fetchData = { account, password }
+  try {
+    isLoading.value = true
+    const response = await axios.post(registerApi, fetchData)
+    if (response.status === 200) {
+      switch (response.data.code) {
+        case 0:
+          message(response.data.message, 'success')
+        // console.log(response.data.message)
+      }
+    }
+    router.push('/signin')
+    // console.log(response)
+  } catch (error: any) {
+    // console.log(error)
+    if (error.response.status === 400) {
+      message(error.response.data.Message, 'error')
+      // console.log(error.response.data.Message)
+    }
+  } finally {
+    isLoading.value = false
+  }
 }
 </script>
 <template>
-  <el-form ref="ruleFormRef" :rules="registerRules" :model="registerInput">
-    <el-form-item label="登入電子信箱帳號:" label-position="top" prop="userEmail">
-      <el-input v-model="registerInput.userEmail" />
+  <el-form ref="ruleFormRef" :rules="registerRules" :model="registerInput" v-loading="isLoading">
+    <el-form-item label="登入電子信箱帳號:" label-position="top" prop="account">
+      <el-input v-model="registerInput.account" />
     </el-form-item>
-    <el-form-item label="密碼:" label-position="top" prop="userPassWord">
-      <el-input v-model="registerInput.userPassWord" />
+    <el-form-item label="密碼:" label-position="top" prop="password">
+      <el-input v-model="registerInput.password" />
     </el-form-item>
     <el-form-item label="請再輸入註冊密碼:" label-position="top" prop="checkPassWord">
       <el-input v-model="registerInput.checkPassWord" />
