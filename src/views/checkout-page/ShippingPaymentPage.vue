b44a225672cf903519bf6616dcdd94ca0d3bf34a
fire0607 <stu00634@gmail.com>
2024-08-21 10:03:27 +0800

    feat:加入購物車進度條元件

--- src/views/checkout-page/ShippingPaymentPage.vue
+++ src/views/checkout-page/ShippingPaymentPage.vue
@@ -1,9 +1,21 @@
 <script setup>
+import { ref } from 'vue'
+import ShoppingCartProgressBar from '@/components/global/ShoppingCartProgressBar.vue'
+
+const steps = ref(['購物車', '填寫資料', '訂單確認'])
+const currentStep = ref(1) 
+
+const nextStep = () => {
+  if (currentStep.value < steps.value.length) {
+    currentStep.value++
+  }
+}
+
 </script>
 <template>
     <div class="grid grid-cols-12 gap-6 md:-mx-3">
         <div class="col-start-5 col-span-4">
-            進度條
+            <ShoppingCartProgressBar :current-step="currentStep" :steps="steps"/>
         </div>
         <div class="col-span-12">
             購物車
@@ -16,5 +28,4 @@
         </div>
     </div>
 </template>
-<style scoped>
-</style>
+<style scoped></style>
