15b172fd64da791675ff476949c9c119009e9a58
AbsintheSung <h90243768@gmail>
2024-08-21 14:30:29 +0800

    feat:配置路由跳轉回到頂部

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,6 +11,9 @@ import CheckoutPage from '@/views/CheckoutPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior() {
+    return { top: 0 }
+  },
   routes: [
     {
       path: '/',
