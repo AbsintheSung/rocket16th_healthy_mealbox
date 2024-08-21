2ac67eed55d7fb3ce317d4b7a37c97190300a72e
AbsintheSung <h90243768@gmail.com>
2024-08-21 23:25:36 +0800

    feat:將MemberPage的列表轉成動態生成

--- src/views/MemberPage.vue
+++ src/views/MemberPage.vue
@@ -1,5 +1,13 @@
 <script setup>
+import { ref } from 'vue'
 import TheSvg from '@/components/global/TheSvg.vue'
+const memberLink = ref([
+  { id: 'editmember', title: '個人資料', path: '/member/editmember' },
+  { id: 'orderhistory', title: '歷史訂單', path: '/member/orderhistory' },
+  { id: 'custommeal', title: '我的自定義餐盒', path: '/member/custommeal' },
+  { id: 'coupon', title: '我的優惠券', path: '/' },
+  { id: 'news', title: '最新消息', path: '/' }
+])
 </script>
 <template>
   <main class="container relative py-20">
@@ -14,41 +22,17 @@ import TheSvg from '@/components/global/TheSvg.vue'
             <p class="text-center text-primary-base">一般會員</p>
           </div>
           <ul class="flex flex-col justify-center">
-            <li class="border-b border-black hover:bg-primary-300">
+            <li
+              class="border-b border-black hover:bg-primary-300"
+              v-for="memberLinkItem in memberLink"
+              :key="memberLinkItem.id"
+            >
               <RouterLink
-                to="/member/editmember"
+                :to="`${memberLinkItem.path}`"
                 class="block px-6 py-4"
                 linkExactActiveClass="link-active"
               >
-                個人資料
-              </RouterLink>
-            </li>
-            <li class="border-b border-black hover:bg-primary-300">
-              <RouterLink
-                to="/member/orderhistory"
-                class="block px-6 py-4"
-                linkExactActiveClass="link-active"
-              >
-                歷史訂單
-              </RouterLink>
-            </li>
-            <li class="border-b border-black hover:bg-primary-300">
-              <RouterLink
-                to="/member/custommeal"
-                class="block px-6 py-4"
-                linkExactActiveClass="link-active"
-              >
-                我的自定義餐盒
-              </RouterLink>
-            </li>
-            <li class="border-b border-black hover:bg-primary-300">
-              <RouterLink to="/" class="block px-6 py-4" linkExactActiveClass="link-active">
-                我的優惠券
-              </RouterLink>
-            </li>
-            <li class="border-b border-black hover:bg-primary-300">
-              <RouterLink to="/" class="block px-6 py-4" linkExactActiveClass="link-active">
-                最新消息
+                {{ memberLinkItem.title }}
               </RouterLink>
             </li>
           </ul>
