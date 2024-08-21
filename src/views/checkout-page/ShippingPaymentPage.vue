9c1245230df8d3af3489b429428ff86665435d6d
fire0607 <stu00634@gmail.com>
2024-08-21 09:36:00 +0800

    feat:加入選擇運送與付款方式頁面基礎布局

--- src/views/checkout-page/ShippingPaymentPage.vue
+++ src/views/checkout-page/ShippingPaymentPage.vue
@@ -1,7 +1,20 @@
 <script setup>
 </script>
 <template>
-    <div>shipping-payment</div>
+    <div class="grid grid-cols-12 gap-6 md:-mx-3">
+        <div class="col-start-5 col-span-4">
+            進度條
+        </div>
+        <div class="col-span-12">
+            購物車
+        </div>
+        <div class="col-span-8">
+            選擇送貨方式及付款方式
+        </div>
+        <div class="col-start-9 col-span-4">
+            選擇送貨方式及付款方式2
+        </div>
+    </div>
 </template>
 <style scoped>
 </style>
