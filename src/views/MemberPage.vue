c62aca12740c35e637de16ad0ec1c4abef3c86cd
AbsintheSung <h90243768@gmail>
2024-08-21 14:14:08 +0800

    update:部分標題樣式配置&連結跳轉

--- src/views/MemberPage.vue
+++ src/views/MemberPage.vue
@@ -17,22 +17,42 @@ import TheSvg from '@/components/global/TheSvg.vue'
             <p class="text-center text-primary-base">一般會員</p>
           </div>
           <ul class="border-block border-block flex flex-col justify-center border">
-            <li class="border-b border-black hover:bg-primary-100">
-              <RouterLink to="/member" class="block px-4 py-6">個人資料</RouterLink>
+            <li class="border-b border-black hover:bg-primary-300">
+              <RouterLink
+                to="/member/editmember"
+                class="block px-4 py-6"
+                linkExactActiveClass="link-active"
+              >
+                個人資料
+              </RouterLink>
             </li>
-            <li class="border-b border-black hover:bg-primary-100">
-              <RouterLink to="/member/editmember" class="block px-4 py-6"> 歷史訂單 </RouterLink>
+            <li class="border-b border-black hover:bg-primary-300">
+              <RouterLink
+                to="/member/orderhistory"
+                class="block px-4 py-6"
+                linkExactActiveClass="link-active"
+              >
+                歷史訂單
+              </RouterLink>
             </li>
-            <li class="border-b border-black hover:bg-primary-100">
-              <RouterLink to="/member/orderhistory" class="block px-4 py-6">
+            <li class="border-b border-black hover:bg-primary-300">
+              <RouterLink
+                to="/member/custommeal"
+                class="block px-4 py-6"
+                linkExactActiveClass="link-active"
+              >
                 我的自定義餐盒
               </RouterLink>
             </li>
-            <li class="border-b border-black hover:bg-primary-100">
-              <RouterLink to="/member" class="block px-4 py-6"> 我的優惠券 </RouterLink>
+            <li class="border-b border-black hover:bg-primary-300">
+              <RouterLink to="/" class="block px-4 py-6" linkExactActiveClass="link-active">
+                我的優惠券
+              </RouterLink>
             </li>
-            <li class="border-b border-black hover:bg-primary-100">
-              <RouterLink to="/member" class="block px-4 py-6"> 最新消息 </RouterLink>
+            <li class="border-b border-black hover:bg-primary-300">
+              <RouterLink to="/" class="block px-4 py-6" linkExactActiveClass="link-active">
+                最新消息
+              </RouterLink>
             </li>
           </ul>
         </aside>
@@ -77,4 +97,8 @@ import TheSvg from '@/components/global/TheSvg.vue'
     </section> -->
   </main>
 </template>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.router-link-active {
+  background-color: $primary-300;
+}
+</style>
