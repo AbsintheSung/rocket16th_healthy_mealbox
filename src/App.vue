b74877e94c3857277643aea7a3597192fa788c60
AbsintheSung <h90243768@gmail>
2024-08-22 17:52:11 +0800

    feat:配置header下方的裝飾物

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import { RouterView } from 'vue-router'
 import HeaderLayout from '@/components/global/HeaderLayout.vue'
 import FooterLayout from '@/components/global/FooterLayout.vue'
+import HeaderAdorn from '@/components/global/HeaderAdorn.vue'
 </script>
 
 <template>
   <HeaderLayout />
+  <HeaderAdorn />
   <RouterView />
   <FooterLayout />
 </template>
