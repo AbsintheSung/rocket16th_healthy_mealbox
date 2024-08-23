a6df77eb3c4907212373712add646cfeea37fa5d
AbsintheSung <h90243768@gmail>
2024-08-23 11:04:32 +0800

    refactor:使用vueUse監聽視窗大小&測試成功

--- src/components/global/HeaderAdorn.vue
+++ src/components/global/HeaderAdorn.vue
@@ -1,25 +1,27 @@
 <script setup>
 import TheSvg from './TheSvg.vue'
+import { useWindowSize } from '@vueuse/core'
 import { ref, onMounted, watch, onUnmounted, computed } from 'vue'
-const windowWidth = ref(window.innerWidth)
-const imgSize = computed(() => Math.ceil(windowWidth.value / 40))
-const updateWindowWidth = () => {
-  windowWidth.value = window.innerWidth
-}
-onMounted(() => {
-  window.addEventListener('resize', updateWindowWidth)
-})
-onUnmounted(() => {
-  window.removeEventListener('resize', updateWindowWidth)
-})
+const { width } = useWindowSize()
+// const windowWidth = ref(window.innerWidth)
+const imgSize = computed(() => Math.ceil(width.value / 40))
+// const updateWindowWidth = () => {
+//   windowWidth.value = window.innerWidth
+// }
+// onMounted(() => {
+//   window.addEventListener('resize', updateWindowWidth)
+// })
+// onUnmounted(() => {
+//   window.removeEventListener('resize', updateWindowWidth)
+// })
 
-watch(
-  () => windowWidth.value,
-  (newWindowWidth) => {
-    windowWidth.value = newWindowWidth
-    // console.log('Window width changed:', newWindowWidth)
-  }
-)
+// watch(
+//   () => windowWidth.value,
+//   (newWindowWidth) => {
+//     windowWidth.value = newWindowWidth
+//     // console.log('Window width changed:', newWindowWidth)
+//   }
+// )
 </script>
 <template>
   <!-- header下方的長條裝飾物  -->
