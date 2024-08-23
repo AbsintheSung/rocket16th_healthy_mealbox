2d81db399c2b22f45e26d8aa2533342d8d726cfd
AbsintheSung <h90243768@gmail>
2024-08-23 11:05:38 +0800

    chore:移除不必要的code

--- src/components/global/HeaderAdorn.vue
+++ src/components/global/HeaderAdorn.vue
@@ -1,27 +1,9 @@
 <script setup>
 import TheSvg from './TheSvg.vue'
 import { useWindowSize } from '@vueuse/core'
-import { ref, onMounted, watch, onUnmounted, computed } from 'vue'
+import { computed } from 'vue'
 const { width } = useWindowSize()
-// const windowWidth = ref(window.innerWidth)
 const imgSize = computed(() => Math.ceil(width.value / 40))
-// const updateWindowWidth = () => {
-//   windowWidth.value = window.innerWidth
-// }
-// onMounted(() => {
-//   window.addEventListener('resize', updateWindowWidth)
-// })
-// onUnmounted(() => {
-//   window.removeEventListener('resize', updateWindowWidth)
-// })
-
-// watch(
-//   () => windowWidth.value,
-//   (newWindowWidth) => {
-//     windowWidth.value = newWindowWidth
-//     // console.log('Window width changed:', newWindowWidth)
-//   }
-// )
 </script>
 <template>
   <!-- header下方的長條裝飾物  -->
