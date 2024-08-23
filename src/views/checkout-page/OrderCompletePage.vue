376afde6edd55c318edfade261bae2cc82e52634
misu13 <djmisu13@gmail.com>
2024-08-23 15:47:00 +0800

    feat:加入訂單完成頁基本樣式

--- src/views/checkout-page/OrderCompletePage.vue
+++ src/views/checkout-page/OrderCompletePage.vue
@@ -1,7 +1,88 @@
 <script setup>
+import { ref } from 'vue'
+import ShoppingCartProgressBar from '@/components/global/ShoppingCartProgressBar.vue'
+const steps = ref(['購物車', '填寫資料', '訂單確認'])
+const currentStep = ref(2)
+const nextStep = () => {
+    if (currentStep.value < steps.value.length) {
+        currentStep.value++
+    }
+}
 </script>
 <template>
-    <div>我是訂單完成頁</div>
+    <div class="grid grid-cols-12 gap-6">
+        <!-- 購物車步驟 -->
+        <div class="col-start-5 col-span-4 pb-4">
+            <ShoppingCartProgressBar :current-step="currentStep" :steps="steps" />
+        </div>
+        <!-- 訂單已成立框框 -->
+        <div class="col-span-12">
+            <div class="flex flex-col justify-center items-center border-2 border-black">
+                <div class="flex justify-center items-center pt-7 pb-3">
+                    <img src="@/assets/image/營養師.svg" alt="logo" class="w-[50px]">
+                    <h2 class="text-2xl text-bold pl-5">謝謝您！您的訂單已成立！</h2>
+                </div>
+                <div class="text-left pl-11 pb-8">
+                    <p>訂單號碼：202407311130088332</p>
+                    <p>訂單確認電郵已發送到您的電子信箱：</p>
+                    <p>hello@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <!-- 訂單資訊框框 -->
+        <div class="col-span-12 pt-6">
+            <div class="flex flex-wrap border-2 border-black p-4">
+                <!-- 訂單資訊 -->
+                <div class="w-full md:w-1/2 px-2 mb-6">
+                    <h2 class="font-bold text-2xl mb-2">訂單資訊</h2>
+                    <div class="flex mb-1">
+                        <span>訂單編號：</span>
+                        <span>202407311130088332</span>
+                    </div>
+                    <div class="flex">
+                        <span>訂單建立時間：</span>
+                        <span>2024-07-31 11:30</span>
+                    </div>
+                </div>
+
+                <!-- 顧客資訊 -->
+                <div class="w-full md:w-1/2 px-2 mb-6">
+                    <h2 class="font-bold text-2xl mb-2">顧客資訊</h2>
+                    <div class="flex mb-1">
+                        <span>姓名：</span>
+                        <span>林飯糰</span>
+                    </div>
+                    <div class="flex">
+                        <span>電話號碼：</span>
+                        <span>222 *** 666</span>
+                    </div>
+                </div>
+
+                <!-- 送貨資訊 -->
+                <div class="w-full md:w-1/2 px-2 mb-6">
+                    <h2 class="font-bold text-2xl mb-2">送貨資訊</h2>
+                    <div class="flex">
+                        <span>送貨方式：</span>
+                        <span>宅配 新竹物流</span>
+                    </div>
+                </div>
+
+                <!-- 付款資訊 -->
+                <div class="w-full md:w-1/2 px-2 mb-6">
+                    <div class="">
+                        <h2 class="font-bold text-2xl mb-2">付款資訊</h2>
+                        <div class="flex mb-1">
+                            <span>付款方式：</span>
+                            <span>信用卡付款</span>
+                        </div>
+                        <div class="flex">
+                            <span>付款狀態：</span>
+                            <span>已付款</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
-<style scoped>
-</style>
+<style scoped></style>
