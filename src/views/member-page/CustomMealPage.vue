ebad92543dba897f0713423c67d1091e566be39f
AbsintheSung <h90243768@gmail>
2024-08-23 13:25:32 +0800

    refactor:微調自定義結構

--- src/views/member-page/CustomMealPage.vue
+++ src/views/member-page/CustomMealPage.vue
@@ -7,25 +7,27 @@ const handleCurrentChange = (val) => {
 }
 </script>
 <template>
-  <h2 class="mb-7 w-fit rounded bg-primary-300 px-4 py-3 text-4xl font-normal shadow-base">
-    自定義餐盒
-  </h2>
-  <ul class="grid grid-cols-3 gap-6">
-    <MealCard v-for="item in 6" :key="item" />
-  </ul>
-  <div class="flex items-center justify-center py-6">
-    <el-pagination
-      style="--el-fill-color: white"
-      layout="prev, pager, next"
-      v-model:current-page="currentPage1"
-      background
-      :page-size="20"
-      :pager-count="11"
-      :total="100"
-      :prev-text="'上一頁'"
-      :next-text="'下一頁'"
-      @current-change="handleCurrentChange"
-    />
+  <div class="flex h-full flex-col">
+    <h2 class="mb-7 w-fit rounded bg-primary-300 px-4 py-3 text-4xl font-normal shadow-base">
+      自定義餐盒
+    </h2>
+    <ul class="grid flex-grow grid-cols-3 gap-6">
+      <MealCard v-for="item in 9" :key="item" />
+    </ul>
+    <div class="flex items-center justify-center py-6">
+      <el-pagination
+        style="--el-fill-color: white"
+        layout="prev, pager, next"
+        v-model:current-page="currentPage1"
+        background
+        :page-size="20"
+        :pager-count="11"
+        :total="100"
+        :prev-text="'上一頁'"
+        :next-text="'下一頁'"
+        @current-change="handleCurrentChange"
+      />
+    </div>
   </div>
 </template>
 <style lang="scss" scoped></style>
