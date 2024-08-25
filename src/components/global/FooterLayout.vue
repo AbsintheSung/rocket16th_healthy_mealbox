21639e3c0cca72f2d0e313e495f16d05e61ac516
AbsintheSung <h90243768@gmail.com>
2024-08-25 21:26:43 +0800

    update:修正footer的在rwd的guid-col數以及圖片警告問題

--- src/components/global/FooterLayout.vue
+++ src/components/global/FooterLayout.vue
@@ -3,7 +3,7 @@ import { RouterLink } from 'vue-router'
 import { ref, computed } from 'vue'
 import { useWindowSize } from '@vueuse/core'
 const { width } = useWindowSize()
-const iconSize = computed(() => (width.value < 576 ? 'md' : 'xl'))
+const iconSize = computed(() => (width.value < 576 ? 'sm' : 'lg'))
 
 const mainNav = ref([
   { id: 'accoumt', title: '我的帳戶', path: '/' },
@@ -19,8 +19,8 @@ const utilNav = ref([
 <template>
   <footer class="bg-primary-700">
     <div class="container py-6">
-      <div class="grid grid-cols-4 gap-6 text-[#FFFFFF] md:grid-cols-12">
-        <ul class="col-span-3 col-start-1 flex text-[12px] md:col-span-6 md:text-base">
+      <div class="grid grid-cols-4 gap-6 text-[#FFFFFF] sm:grid-cols-12">
+        <ul class="col-span-3 col-start-1 flex text-[12px] sm:col-span-9 sm:text-base">
           <li v-for="mainNavItem in mainNav" :key="mainNavItem.id" class="py-1">
             <RouterLink class="block px-2" :to="`${mainNavItem.path}`" :id="mainNavItem.id">
               {{ mainNavItem.title }}
@@ -28,7 +28,7 @@ const utilNav = ref([
           </li>
         </ul>
         <ul
-          class="col-span-1 col-start-4 flex items-center justify-end md:col-span-6 md:col-start-7"
+          class="col-span-1 col-start-4 flex items-center justify-end sm:col-span-3 sm:col-start-10"
         >
           <li v-for="utilNavItem in utilNav" :key="utilNavItem.id" class="py-1">
             <RouterLink class="block px-2" :to="`${utilNavItem.path}`">
