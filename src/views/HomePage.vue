192da7bb3d5fdf663b9d26f95cc16c80051fe5b7
AbsintheSung <h90243768@gmail>
2024-08-28 10:29:38 +0800

    feat:配置首頁大圖

---
+++ src/views/HomePage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <section class="bg-home-main bg-cover bg-center bg-no-repeat py-72"></section>
+  </main>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
     },
     container: {
       center: true,
-      padding: '0.75rem',//16*0.75=12
+      padding: '0.75rem', //16*0.75=12
       screens: {
         ssm: '100%',
         sm: '100%',
@@ -22,37 +22,40 @@ export default {
       }
     },
     fontFamily: {
-      "Noto-Sans-TC": ["Noto Sans TC", "sans-serif"],
+      'Noto-Sans-TC': ['Noto Sans TC', 'sans-serif']
     },
     extend: {
+      backgroundImage: {
+        'home-main': "url('@/assets/image/homemain.jpg')"
+      },
       colors: {
         primary: {
-          base: "#58C067",
-          50: "#F2FBF3",
-          100: "#E2F6E5",
-          200: "#C5EDCA",
-          300: "#98DDA2",
-          400: "#58C067",
-          500: "#3EA94D",
-          600: "#2E8B3B",
-          700: "#286D33",
-          800: "#24572C",
-          900: "#1F4826",
-          950: "#0C2711"
+          base: '#58C067',
+          50: '#F2FBF3',
+          100: '#E2F6E5',
+          200: '#C5EDCA',
+          300: '#98DDA2',
+          400: '#58C067',
+          500: '#3EA94D',
+          600: '#2E8B3B',
+          700: '#286D33',
+          800: '#24572C',
+          900: '#1F4826',
+          950: '#0C2711'
         },
         secondary: {
-          base: "#FA8B6F",
-          50: "#FEF4F2",
-          100: "#FEE8E2",
-          200: "#FFD4C9",
-          300: "#FDB6A4",
-          400: "#FA8B6F",
-          500: "#F26541",
-          600: "#DF4923",
-          700: "#BB3A1A",
-          800: "#9B3319",
-          900: "#80301C",
-          950: "#461509"
+          base: '#FA8B6F',
+          50: '#FEF4F2',
+          100: '#FEE8E2',
+          200: '#FFD4C9',
+          300: '#FDB6A4',
+          400: '#FA8B6F',
+          500: '#F26541',
+          600: '#DF4923',
+          700: '#BB3A1A',
+          800: '#9B3319',
+          900: '#80301C',
+          950: '#461509'
         },
         success: {
           base: '#67c23a'
@@ -68,10 +71,10 @@ export default {
         },
         info: {
           base: '#909399'
-        },
+        }
       },
       boxShadow: {
-        'base': '5px 5px black',
+        base: '5px 5px black'
       }
     }
   },
