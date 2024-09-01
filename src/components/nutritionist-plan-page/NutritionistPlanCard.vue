b5b581e949e0e5b230a43274061ae4242ed29c7c
misu13 <djmisu13@gmail.com>
2024-09-01 19:58:32 +0800

    refactor:調整營養師方案選擇頁單張卡片樣式

--- src/components/nutritionist-plan-page/NutritionistPlanCard.vue
+++ src/components/nutritionist-plan-page/NutritionistPlanCard.vue
@@ -7,15 +7,23 @@ defineProps({
 </script>
 <template>
   <div class="flex flex-col">
-    <h3 class="text-2xl font-medium pb-6">{{ title }}</h3>
     <div class="w-full">
-      <img :src="src" :alt="title" class="w-full h-[179px] object-cover">
-      <div class="flex justify-end items-center bg-secondary-50 py-4">
-        <div class="pr-4"><button class="bg-secondary-base py-2 px-8 border-2 border-black rounded-md hover:shadow-base transition active:shadow-none">加入購物車</button></div>
-        <RouterLink class="flex items-center text-2xl text-black hover:text-secondary-500 transition" to="/nutritionist-plan-detail">
-          <p class="pr-4">查看詳細</p><font-awesome-icon :icon="['fas', 'arrow-right']" class="pr-4" />
+      <div class="border-2 border-black rounded-t"><img :src="src" :alt="title" class="w-full h-[200px] object-cover">
+      </div>
+      <div class="flex flex-col items-start border-2 border-black rounded-b p-3">
+        <RouterLink to="/nutritionist-plan-detail">
+          <h3 class="text-xl hover:text-primary-600 transition">{{ title }}</h3>
         </RouterLink>
-
+        <div class="py-2">
+          <p class="text-sm text-gray-400">Display richer content by adding some configs.</p>
+          <p class="text-sm text-gray-400">Display richer content by adding some configs.</p>
+        </div>
+        <div class="pr-4">
+          <button
+            class="py-1 px-4 border-2 border-secondary-900 rounded-md hover:shadow-base transition active:shadow-none">
+            <p class="text-secondary-900">加入購物車</p>
+          </button>
+        </div>
       </div>
     </div>
   </div>
