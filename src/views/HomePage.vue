58929587c2d46e18f29de8a4d546125faa2e3a53
AbsintheSung <h90243768@gmail>
2024-09-02 12:46:55 +0800

    feat:配置首頁-介紹區塊-2

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -295,6 +295,23 @@ const swiperOptions = {
         </div>
       </div>
     </section>
+
+    <!-- 首頁-介紹區塊-2 -->
+    <section>
+      <ThePlaidAdorn />
+
+      <div class="container py-20">
+        <div class="flex flex-col items-center justify-center">
+          <TheSvg svgIcon="main-logo" class="h-[50px] w-[340px]" />
+          <h2 class="my-5 text-3xl font-bold text-primary-700">「又懶又健康」</h2>
+          <p>這是 盒食 的宗旨，健康是財富之本，我們希望無論您的生活多麼忙碌都能吃得營養均衡。</p>
+          <p>無論您是忙碌的上班族、辛苦的全職媽媽，或者您與我們一樣，是個又懶又想吃的健康的人，</p>
+          <p class="text-center">盒食提供您日常飲食更多元的選擇，健康美味輕鬆上桌。</p>
+        </div>
+      </div>
+      <ThePlaidAdorn />
+    </section>
+    <TheNewsTicker />
     <!-- <section class="container py-12">
       <h2 class="text-3xl font-bold text-primary-700 md:text-5xl lg:text-7xl">
         「想要健康，就要吃得均衡。」
