f16ee77c78824e3d05315db3b9b1efeae99ded87
misu13 <djmisu13@gmail.com>
2024-09-03 16:49:15 +0800

    update:更新基礎營養師方案選擇卡片樣式

--- src/components/plan-selection-page/PlanSelectionCard.vue
+++ src/components/plan-selection-page/PlanSelectionCard.vue
@@ -4,26 +4,28 @@
     <!-- 方案選擇區塊 -->
     <div class="grid grid-cols-4 gap-3 md:grid-cols-12 md:gap-6">
         <!-- 營養師方案 -->
-        <section class="flex flex-col justify-evenly col-span-4 md:col-start-3">
-            <div class="border-2 border-black rounded-t bg-primary-300">
-                <ul class="flex gap-2 px-4 py-3">
+        <section class="flex flex-col col-span-4 md:col-start-3">
+            <!-- 卡片上方欄位 -->
+            <div class="flex justify-between items-center border-2 border-black rounded-t bg-primary-300 px-4 py-3">
+                <ul class="flex gap-2">
                     <li class="border-2 border-black rounded-full h-4 w-4 bg-[#F15159]"></li>
                     <li class="border-2 border-black rounded-full h-4 w-4 bg-[#FDCF00]"></li>
                     <li class="border-2 border-black rounded-full h-4 w-4 bg-[#2DD46F]"></li>
                 </ul>
+                <h3 class="font-bold md:hidden">營養師方案</h3>
             </div>
-            <div class="border-2 border-black rounded-b py-9">
-                <div class="flex justify-center">
-                    <TheSvg svgIcon="nutritionist-icon" class="mt-auto hidden w-[100px] h-[108px] md:block" />
+            <!-- 營養師方案卡片 -->
+            <div class="flex flex-col items-center justify-center border-2 border-black rounded-b py-9">
+                <div class="text-center flex justify-center items-center border-2 border-black rounded-full w-20 h-20 mb-4">
+                    <TheSvg svgIcon="nutritionist-icon" class="w-[50px] h-[50px]" />
                 </div>
-                <div class="relative">
-                    <h3 class="py-12 text-3xl font-medium text-center">營養師方案</h3>
-                    <!-- 裝飾性圖片 -->
+                <!-- <div class="relative">
+                    <h3 class="py-12 text-2xl font-bold text-center">營養師方案</h3>
                     <TheSvg svgIcon="hendecagon"
-                        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-auto hidden w-[250px] -z-10 md:block" />
-                </div>
-                <p class="text-center mb-4">由專業營養師依照成人一日所需營養素搭配餐盒</p>
-                <div class="flex flex-row flex-nowrap justify-center pb-16">
+                        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-auto hidden w-[290px] -z-10 md:block" />
+                </div> -->
+                <p class="text-center mb-4 font-medium">由專業營養師依照成人<br>一日所需營養素搭配餐盒</p>
+                <div class="flex flex-row flex-nowrap justify-center pb-12">
                     <ul class="space-y-2">
                         <li>
                             <p><span class="text-primary-base pr-1"><font-awesome-icon
@@ -46,7 +48,7 @@
                 </div>
                 <div class="flex justify-center items-center">
                     <RouterLink
-                        class="text-center w-4/5 py-5 border-2 border-black bg-secondary-base rounded hover:shadow-base transition active:shadow-none"
+                        class="text-center py-5 px-28 border-2 border-secondary-900 rounded hover:border-2 hover:bg-secondary-400 hover:border-black hover:shadow-base hover:transition active:shadow-none"
                         to="/nutritionist-plan">
                         <p>直接訂購</p>
                     </RouterLink>
@@ -55,13 +57,16 @@
         </section>
         <!-- 自定義方案 -->
         <section class="flex flex-col justify-evenly col-span-4 md:col-start-7">
-            <div class="border-2 border-black rounded-t bg-primary-300">
-                <ul class="flex gap-2 px-4 py-3">
+            <!-- 卡片上方欄位 -->
+            <div class="border-2 border-black rounded-t bg-primary-300 px-4 py-3">
+                <ul class="flex gap-2">
                     <li class="border-2 border-black rounded-full h-4 w-4 bg-[#F15159]"></li>
                     <li class="border-2 border-black rounded-full h-4 w-4 bg-[#FDCF00]"></li>
                     <li class="border-2 border-black rounded-full h-4 w-4 bg-[#2DD46F]"></li>
                 </ul>
+                <h3 class="font-bold md:hidden">自定義方案</h3>
             </div>
+            <!-- 自定義方案卡片 -->
             <div class="border-2 border-black rounded-b py-9">
                 <div class="flex justify-center">
                     <TheSvg svgIcon="customize-icon" class="mt-auto hidden w-[100px] h-[108px] md:block" />
