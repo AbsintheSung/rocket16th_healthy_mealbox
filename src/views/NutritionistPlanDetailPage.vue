bc934c5358b0bd0169acf59b25c5d8bc67222400
misu13 <djmisu13@gmail.com>
2024-09-05 00:00:57 +0800

    feat:獲取營養師套餐個別API資料

--- src/views/NutritionistPlanDetailPage.vue
+++ src/views/NutritionistPlanDetailPage.vue
@@ -1,11 +1,21 @@
 <script setup>
-import NutritionistPlanDetailCard from '@/components/nutritionist-plan-detail-page/NutritionistPlanDetailCard.vue'
+import { computed,onMounted } from 'vue'
+import { useNutritionistPlanStore } from '@/stores/nutritionistPlan'
 import NutritionistPlanDetailInfo from '@/components/nutritionist-plan-detail-page/NutritionistPlanDetailInfo.vue'
+import NutritionistPlanDetailCard from '@/components/nutritionist-plan-detail-page/NutritionistPlanDetailCard.vue'
+const nutritionistPlanStore = useNutritionistPlanStore()
+const onePlanData = computed(() => nutritionistPlanStore.getOneNutritionistPlan)
+
+onMounted(async () => {
+  await nutritionistPlanStore.fetchOneNutritionistPlan(route.params.id)
+})
 </script>
 <template>
     <div class="container py-14">
-        <NutritionistPlanDetailInfo />
-        <NutritionistPlanDetailCard />
+        <NutritionistPlanDetailInfo/>
+        <NutritionistPlanDetailCard/>
+        <!-- <NutritionistPlanDetailInfo />
+        <NutritionistPlanDetailCard /> -->
     </div>
 </template>
 <style scoped>
