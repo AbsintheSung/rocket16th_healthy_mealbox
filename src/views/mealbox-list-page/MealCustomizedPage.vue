2b8067bd6d7ba87f6dccb9954d9d562b5b24d8fe
AbsintheSung <h90243768@gmail>
2024-09-05 10:43:42 +0800

    refactor:是得自定義按鈕會跟卡片等高

--- src/views/mealbox-list-page/MealCustomizedPage.vue
+++ src/views/mealbox-list-page/MealCustomizedPage.vue
@@ -9,10 +9,10 @@ const customMealBoxStore = useCustomMealBoxStore()
   <div>
     <ul
       v-if="customMealBoxStore.getDataTotal > 0"
-      class="grid grid-cols-2 gap-x-6 gap-y-3 md:grid-cols-3 md:gap-y-12 lg:grid-cols-4"
+      class="custom-mealcard-row grid grid-cols-2 gap-x-6 gap-y-3 md:grid-cols-3 md:gap-y-12 lg:grid-cols-4"
     >
       <MealCard v-for="item in customMealBoxStore.getPaginatedMeals" :key="item" />
-      <li class="flex flex-col gap-y-4 rounded border border-dashed p-4">
+      <li class="flex h-full flex-col gap-y-4 rounded border border-dashed p-4">
         <button class="flex flex-grow flex-col items-center justify-center gap-y-6">
           <fontAwesomeIcon :icon="['fas', 'plus']" class="p-2" />
           <p>新增餐盒</p>
@@ -44,4 +44,14 @@ const customMealBoxStore = useCustomMealBoxStore()
     </div>
   </div>
 </template>
-<style scoped></style>
+<style scoped lang="scss">
+.custom-mealcard-row {
+  /* 
+  因為 自定義區塊 有一個卡片按鈕是 新增餐盒，它必須和其他卡片一樣的高度，
+  當該按鈕，在最下方且周圍沒有其他 li 卡片的話，會導致它無法跟有內容的卡片高度一致，
+  使得它高度為原始高度。
+  可以直接在 ul 添加 grid-auto-rows-fr ，為了註解並解釋此 css設定所以另外寫
+  */
+  grid-auto-rows: 1fr;
+}
+</style>
