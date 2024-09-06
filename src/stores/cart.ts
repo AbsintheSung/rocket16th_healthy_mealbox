b36e1181232838aeb5cf9318b8369132082f20ea
AbsintheSung <h90243768@gmail>
2024-09-06 09:58:08 +0800

    update:將DaySelectionButton.vue改成ts，並添加部分邏輯和store設定

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -3,10 +3,22 @@ import { fetchApi } from '@/utils/api/apiUrl'
 import { ref, computed } from 'vue'
 export const useCartStore = defineStore('cart', () => {
   //State
+  const caseType = ref<Number>(7) //caseType的原始資料
 
   //Getter
 
+  //取得caseType的資訊
+  const getCaseType = computed(() => caseType.value)
+
   //Action
 
-  return {}
+  //修改caseType調用此function
+  const changeSelectPlan = (planDay: Number) => {
+    caseType.value = planDay
+  }
+
+  return {
+    getCaseType,
+    changeSelectPlan
+  }
 })
