d7fb99b627f34d8d5e961323a571021c1d0715f7
AbsintheSung <h90243768@gmail>
2024-09-06 09:46:30 +0800

    update:因為格式化設定不同，此處先存檔格式化DaySelectionButton.vue檔案

--- src/components/global/DaysSelectionButton.vue
+++ src/components/global/DaysSelectionButton.vue
@@ -1,25 +1,32 @@
 <script setup>
 import { ref } from 'vue'
 const options = [
-    { value: 7, label: '7餐' },
-    { value: 14, label: '14餐' },
-    { value: 21, label: '21餐' },
+  { value: 7, label: '7餐' },
+  { value: 14, label: '14餐' },
+  { value: 21, label: '21餐' }
 ]
 const selectedPlan = ref(null)
 const selectPlan = (value) => {
-    selectedPlan.value = value
-    // 其他篩選邏輯待補
-    console.log(value)
+  selectedPlan.value = value
+  // 其他篩選邏輯待補
+  console.log(value)
 }
 </script>
 <template>
-    <div class="bg-primary-base flex justify-evenly py-5 md:py-2">
-        <button v-for="option in options" :key="option.value"
-            :class="['text-2xl md:text-4xl text-primary-700 font-bold', 
-            selectedPlan === option.value ? 'text-2xl md:text-4xl text-white font-bold transition-all':'text-2xl md:text-4xl text-primary-700 font-bold']"
-            @click=" selectPlan(option.value)">
-            {{ option.label }}
-        </button>
-    </div>
+  <div class="flex justify-evenly bg-primary-base py-5 md:py-2">
+    <button
+      v-for="option in options"
+      :key="option.value"
+      :class="[
+        'text-2xl font-bold text-primary-700 md:text-4xl',
+        selectedPlan === option.value
+          ? 'text-2xl font-bold text-white transition-all md:text-4xl'
+          : 'text-2xl font-bold text-primary-700 md:text-4xl'
+      ]"
+      @click="selectPlan(option.value)"
+    >
+      {{ option.label }}
+    </button>
+  </div>
 </template>
 <style scoped></style>
