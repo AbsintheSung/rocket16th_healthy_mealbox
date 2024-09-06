751338e11cb01135eca0c834ad0158ad1a7f5872
AbsintheSung <h90243768@gmail>
2024-09-06 09:42:27 +0800

    feat:建立cart的store

---
+++ src/stores/cart.ts
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia'
+import { fetchApi } from '@/utils/api/apiUrl'
+import { ref, computed } from 'vue'
+export const useCartStore = defineStore('cart', () => {
+  //State
+
+  //Getter
+
+  //Action
+
+  return {}
+})
