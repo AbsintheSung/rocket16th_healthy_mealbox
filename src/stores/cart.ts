9cbf6b73de3d12779309d6b9879934cb8caf6ba8
AbsintheSung <h90243768@gmail>
2024-09-06 10:43:43 +0800

    chore:添加註解

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -3,18 +3,20 @@ import { fetchApi } from '@/utils/api/apiUrl'
 import { ref, computed } from 'vue'
 import type { GeneralBoxes } from '@/types/cartType'
 export const useCartStore = defineStore('cart', () => {
-  //State
+  /* States */
   const caseType = ref<Number>(7) //caseType的原始資料
   const generalBoxes = ref([]) //存放獲取後的一般餐盒
   const customizeBoxes = ref([]) //存放獲取後的自定義餐盒
 
-  //Getter
+  /* Getter */
 
   //取得caseType的資訊
   const getCaseType = computed(() => caseType.value)
+
+  //取得目前會員已加入購物車商品總數
   const getMealBoxTotal = computed(() => generalBoxes.value.length + customizeBoxes.value.length)
 
-  //Action
+  /* Action */
 
   //修改caseType調用此function
   const changeSelectPlan = (planDay: Number) => {
