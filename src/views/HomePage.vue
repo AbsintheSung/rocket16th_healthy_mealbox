5248cf923a6a6d12ffc0a92f6c309e5921e21315
AbsintheSung <h90243768@gmail.com>
2024-09-09 00:32:05 +0800

    update:新增-首頁-網站介紹-1-rwd

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -42,15 +42,15 @@ const swiperOptions = {
   <main>
     <!-- 首頁大圖 -->
     <section class="bg-home-main bg-cover bg-center bg-no-repeat">
-      <div class="container flex items-center justify-between">
-        <div class="w-1/2 py-48">
+      <div class="container flex flex-col items-center justify-between sm:flex-row">
+        <div class="w-full py-48 sm:w-1/2">
           <TheSvg svgIcon="main-logo" class="h-[90px]" />
           <p class="text-2xl text-white">
             無論您的生活多麼忙碌都能吃得營養均衡，輕鬆快速訂購，<br />
             盒食提供您日常飲食更多元的選擇，健康美味輕鬆上桌!
           </p>
         </div>
-        <div class="flex w-1/2 justify-end self-end">
+        <div class="flex w-full justify-end self-end sm:w-1/2">
           <TheSvg svgIcon="rhombus-full" class="w-[80px]" />
           <TheSvg svgIcon="rhombus" class="w-[80px]" />
           <TheSvg svgIcon="rhombus-full" class="w-[80px]" />
@@ -108,8 +108,8 @@ const swiperOptions = {
     </section>
 
     <!-- 首頁-網站介紹-1 -->
-    <section class="hidden bg-background-style-2 lg:block">
-      <div class="container py-14">
+    <section class="bg-background-style-2">
+      <div class="container hidden py-14 md:block">
         <h2 class="text-center text-5xl font-bold text-primary-700">
           「想要健康，就要吃得均衡。」
         </h2>
@@ -178,6 +178,86 @@ const swiperOptions = {
                 <img src="../assets/image/測試義大利麵.png" />
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- rwd版 -->
+      <div class="container py-12 md:hidden">
+        <h2 class="text-center text-2xl font-bold text-primary-700">
+          「想要健康，就要吃得均衡。」
+        </h2>
+        <div class="grid grid-cols-4 gap-x-6">
+          <div class="col-span-full">
+            <div class="flex justify-between">
+              <div class="flex items-end">
+                <img src="../assets//image/測試沙拉1.png" alt="沙拉圖片" class="mt-5 w-1/2" />
+              </div>
+              <div class="flex flex-col">
+                <img src="../assets//image/測試沙拉3.png" alt="沙拉圖片" class="ms-auto w-1/2" />
+                <div class="mt-auto flex">
+                  <TheSvg svgIcon="rhombus-full" class="h-[40px] w-[30px]" />
+                  <TheSvg svgIcon="rhombus" class="h-[40px] w-[30px]" />
+                  <TheSvg svgIcon="rhombus-full" class="h-[40px] w-[30px]" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-full flex flex-col gap-y-2">
+            <div class="flex items-center gap-x-5">
+              <p class="flex h-full flex-col justify-around text-xl">
+                <span>澱</span><span>粉</span>
+              </p>
+              <h3 class="text-3xl font-medium">15g</h3>
+            </div>
+            <p>
+              澱粉是推動卡路里的重要能源 不是極端的澱粉限制,而是適當的攝取量，選擇盒食，
+              可以讓您精確的澱粉控制，在澱粉控制中，從食材調配的苦惱中 解放出來，度過悠閒的用餐時光
+            </p>
+          </div>
+          <div class="col-span-full grid grid-cols-4 gap-x-6">
+            <div class="col-span-2 mt-10">
+              <div class="flex flex-col gap-y-2">
+                <img src="../assets//image/測試沙拉2.png" alt="沙拉圖片" />
+                <div class="flex">
+                  <TheSvg svgIcon="rhombus-full" class="h-[40px] w-[30px]" />
+                  <TheSvg svgIcon="rhombus" class="h-[40px] w-[30px]" />
+                  <TheSvg svgIcon="rhombus-full" class="h-[40px] w-[30px]" />
+                </div>
+                <div class="col-span-5 col-start-2 flex items-center gap-x-5">
+                  <p class="flex h-full flex-col justify-around text-xl">
+                    <span>熱</span><span>量</span>
+                  </p>
+                  <h3 class="text-3xl font-medium">350Kcal</h3>
+                </div>
+              </div>
+            </div>
+            <div class="col-span-2">
+              <div class="flex flex-col gap-y-2">
+                <img
+                  src="../assets//image/測試義大利麵.png"
+                  alt="義大利麵圖片"
+                  class="ms-auto w-1/2"
+                />
+                <div class="col-span-full flex items-center gap-x-5">
+                  <p class="flex h-full flex-col justify-around text-xl">
+                    <span>蛋</span><span>白</span><span>質</span>
+                  </p>
+                  <h3 class="text-3xl font-medium">20g</h3>
+                </div>
+                <p>
+                  人體組成除了水分和脂質之外， 大部分都是蛋白質構成的以平均 成人攝取1餐推薦的蛋白質
+                  爲標準,肉、海鮮、雞蛋、豆類 等食材爲蛋白質來源
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-full">
+            <p class="py-2">
+              以平均成年人一天的適當熱量爲基準,由營養管理師監督所有食譜
+              不要盲目地減少卡路里，重要的是要攝取多種食材，
+              營養均衡的飲食用正確的知識面對卡路里對於維持健康、愉快、 美味的健康生活非常重要！
+            </p>
           </div>
         </div>
       </div>
