0b4ba32a1243012ee53994436f69a1703463b992
AbsintheSung <h90243768@gmail.com>
2024-09-09 12:34:25 +0800

    feat:添加取得價錢資訊並存入store

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -2,12 +2,17 @@ import { defineStore } from 'pinia'
 import { fetchApi } from '@/utils/api/apiUrl'
 import { ref, computed } from 'vue'
 import type { CartGeneralMealBoxes } from '@/types/type'
+type CartInfo = {
+  prize: number;
+  freightFree: boolean;
+  expirationDate: string;
+};
 export const useCartStore = defineStore('cart', () => {
   /* States */
   const caseType = ref<number>(7) //caseType的原始資料
   const generalBoxes = ref<CartGeneralMealBoxes[]>([]) //存放獲取後的一般餐盒
   const customizeBoxes = ref([]) //存放獲取後的自定義餐盒
-
+  const cartInfo = ref<Partial<CartInfo>>({}); //存放購物車 價錢 優惠資訊
   /* Getter */
 
   //取得caseType的資訊
@@ -31,6 +36,11 @@ export const useCartStore = defineStore('cart', () => {
       composition: { ...item.composition }, // 深拷貝 composition
       imgArr: [...item.imgArr], // 深拷貝 imgArr
     }));
+  })
+
+  //取的購物車 價格資訊
+  const getCartInfo = computed(() => {
+    return { ...cartInfo.value }
   })
 
   /* Action */
@@ -67,6 +77,11 @@ export const useCartStore = defineStore('cart', () => {
       if (response.status === 200) {
         generalBoxes.value = response.data.data.generalBoxes
         customizeBoxes.value = response.data.data.customizeBoxes
+        cartInfo.value = {
+          prize: response.data.data.prize,
+          freightFree: response.data.data.freightFree,
+          expirationDate: response.data.data.expirationDate,
+        }
       }
     } catch (error) {
       console.log(error)
@@ -86,9 +101,14 @@ export const useCartStore = defineStore('cart', () => {
         boxQuantity: addMealBoxQuantity(id) // 目前此商品要這個數量，0 的話代表購物車移除此商品
       }
       const response = await fetchApi.updateCart(mealData)
-      if (response.status) {
+      if (response.status === 200) {
         generalBoxes.value = response.data.data.generalBoxes
         customizeBoxes.value = response.data.data.customizeBoxes
+        cartInfo.value = {
+          prize: response.data.data.prize,
+          freightFree: response.data.data.freightFree,
+          expirationDate: response.data.data.expirationDate,
+        }
         // await fetchMemberCartInfo()
         return response.data
       }
@@ -110,9 +130,14 @@ export const useCartStore = defineStore('cart', () => {
         boxQuantity: minusMealBoxQuantity(id) // 目前此商品要這個數量，0 的話代表購物車移除此商品
       }
       const response = await fetchApi.updateCart(mealData)
-      if (response.status) {
+      if (response.status === 200) {
         generalBoxes.value = response.data.data.generalBoxes
         customizeBoxes.value = response.data.data.customizeBoxes
+        cartInfo.value = {
+          prize: response.data.data.prize,
+          freightFree: response.data.data.freightFree,
+          expirationDate: response.data.data.expirationDate,
+        }
         // await fetchMemberCartInfo()
         return response.data
       }
@@ -125,6 +150,7 @@ export const useCartStore = defineStore('cart', () => {
     getCaseType,
     getMealBoxTotal,
     getGeneralBoxes,
+    getCartInfo,
     changeSelectPlan,
     fetchMemberCartInfo,
     fetchaddGeneralCart,
