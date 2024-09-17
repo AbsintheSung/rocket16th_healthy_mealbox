895cbd518f133e0653ad482111207b7d12804e3f
AbsintheSung <h90243768@gmail.com>
2024-09-17 16:39:12 +0800

    chore:移除用不到的code跟註解

--- src/views/SingleMealPage.vue
+++ src/views/SingleMealPage.vue
@@ -6,34 +6,8 @@ import TheIngredient from '@/components/global/TheIngredient.vue'
 const generalMealBoxStore = useGeneralMealBoxStore()
 const route = useRoute()
 const router = useRouter()
-const drawer = ref(false)
+// const drawer = ref(false)
 const oneMealData = computed(() => generalMealBoxStore.getOneGeneralMeal)
-//英文對應中文
-// const nutrientNameMap = {
-//   calories: '卡路里',
-//   protein: '蛋白質',
-//   adipose: '脂肪',
-//   carbohydrate: '碳水化合物',
-//   fiber: '纖維',
-//   sodium: '鈉含量'
-// }
-//將物件轉換為數組並添加單位
-// const nutritionItems = computed(() => {
-//   if (!oneMealData.value || !oneMealData.value.composition) return []
-
-//   return Object.entries(oneMealData.value.composition).map(([key, value]) => ({
-//     name: nutrientNameMap[key] || key,
-//     value: `${value}${key === 'calories' ? 'kcal' : 'g'}`
-//   }))
-// })
-// const nutritionItems = [
-//   { name: '卡路里', value: '300kcal' },
-//   { name: '蛋白質', value: '20g' },
-//   { name: '脂肪', value: '12g' },
-//   { name: '碳水化合物', value: '300kcal' },
-//   { name: '糖', value: '15g' },
-//   { name: '纖維', value: '3.1g' }
-// ]
 const getDishList = computed(() => {
   return [
     {
@@ -106,47 +80,9 @@ onMounted(async () => {
             </li>
           </ul>
         </div>
-        <!-- <div class="col-span-full col-start-1">
-          <ul class="flex w-full flex-wrap items-center py-6 md:flex-nowrap">
-            <li
-              v-for="(item, index) in nutritionItems"
-              :key="item.name"
-              class="flex w-1/3 flex-col items-center justify-center gap-y-4 border-black py-2 md:py-0"
-              :class="[
-                index < 3 ? 'border-b md:border-b-0' : 'border-t md:border-t-0',
-                index % 3 === 0 ? 'md:border-l' : 'border-l',
-                (index + 1) % 3 === 0 ? 'border-r-0' : 'border-r',
-                'md:border-x',
-                { 'md:first:border-l-0': index === 0 },
-                { 'md:last:border-r-0': index === nutritionItems.length - 1 }
-              ]"
-            >
-              <h3>{{ item.name }}</h3>
-              <p>{{ item.value }}</p>
-            </li>
-          </ul>
-        </div> -->
       </div>
       <div class="col-span-full col-start-1">
         <TheIngredient :allDishList="getDishList" />
-        <!-- <ul class="flex w-full flex-wrap items-center py-6 md:flex-nowrap">
-          <li
-            v-for="(item, index) in nutritionItems"
-            :key="item.name"
-            class="flex w-1/3 flex-col items-center justify-center gap-y-4 border-black py-2 md:py-0"
-            :class="[
-              index < 3 ? 'border-b md:border-b-0' : 'border-t md:border-t-0',
-              index % 3 === 0 ? 'md:border-l' : 'border-l',
-              (index + 1) % 3 === 0 ? 'border-r-0' : 'border-r',
-              'md:border-x',
-              { 'md:first:border-l-0': index === 0 },
-              { 'md:last:border-r-0': index === nutritionItems.length - 1 }
-            ]"
-          >
-            <h3>{{ item.name }}</h3>
-            <p>{{ item.value }}</p>
-          </li>
-        </ul> -->
       </div>
     </section>
   </main>
