d8337193b95f48f982618daecc624753ce2c2b01
fire0607 <stu00634@gmail.com>
2024-09-20 02:48:47 +0800

    feat:新增linepay轉跳中間頁

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,6 +12,7 @@ import EditCustomMealPage from '@/views/EditCustomMealPage.vue'
 import CustomSinglePage from '@/views/CustomSinglePage.vue'
 import FqaPage from '@/views/FqaPage.vue'
 import HowToUsePage from '@/views/HowToUsePage.vue'
+import LinepayRedirectPage from '@/views/LinepayRedirect.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -156,6 +157,11 @@ const router = createRouter({
       path: '/how-to-use',
       name:'HowToUseP',
       component: HowToUsePage
+    },
+    {
+      path: '/linepay-redirect',
+      name:'LinePayRedirect',
+      component: LinepayRedirectPage
     },
     //配置404 ( 此配置在最下面，之後新增的路由請新增在此路由設定上面 )
     {
